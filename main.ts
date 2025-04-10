0a7031271d7635b321b50d13ac5a9f989ba389ec
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2025-04-10 13:25:50 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
+// This will move the position of the servo to create tick marks to measure velocity
 function move_servo(position_1: number, position_2: number, time_1: number, time_2: number) {
-    //  80, 135, 50, 950
     pins.servoWritePin(AnalogPin.P15, position_1)
     basic.pause(time_1)
     pins.servoWritePin(AnalogPin.P15, position_2)
     basic.pause(time_2)
 }
 
+// This will move the robot at a determined speed.
 function move_robot(speed: number) {
-    //  50
     Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
 }
 
+// The robot will move at determined speed while leaving tick marks to indicate distance per second.
+// The robot will provide 5 tick marks unless loop in increased or decreased.
+// Touch the MicroBit logo to run the main program with 5 tick marks.
 input.onLogoEvent(TouchButtonEvent.Pressed, function main() {
     move_robot(25)
-    while (true) {
-        move_servo(80, 135, 50, 950)
+    for (let i = 0; i < 5; i++) {
+        move_servo(0, 180, 200, 800)
     }
+    move_robot(0)
 })
